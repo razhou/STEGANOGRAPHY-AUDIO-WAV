83e8928df77701d50610d3ef507b18ada3f0fa92
Raju Riyanda <r4ju_riy4nd4@yahoo.co.id>
2013-01-18 01:28:22 +0700

    Form sembunyi pesan
    Form sembunyi pesan

--- STEGANOGRAPHY WAV/STEGANOGRAPHY WAV/Form1.cs
+++ STEGANOGRAPHY WAV/STEGANOGRAPHY WAV/Form1.cs
@@ -16,7 +16,61 @@ namespace STEGANOGRAPHY_WAV
             InitializeComponent();
         }
 
+        private void browsebutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            GetFileName(dlg, browsefield, true);
+        }
+        private void GetFileName(FileDialog dialog, TextBox control, bool useFilter)
+        {
+            if (useFilter) { dialog.Filter = "Wave Audio (*.wav)|*.wav"; }
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                control.Text = dialog.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browsefield_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
